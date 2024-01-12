ba2f34d379b73bb0e32cafde8a3ba8063bc68194
“Lenn-e” <theiign@gmail.com>
2024-01-12 20:39:18 +0100

    Add faucet status

--- src/components/Faucet/Faucet.js
+++ src/components/Faucet/Faucet.js
@@ -1,8 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import EthereumContext from '../../EthereumContext';
 import './Faucet.css';
 
 function Faucet() {
+    const [status, setStatus] = useState("");
     const { signer } = useContext(EthereumContext);
 
     const handleFaucet = async () => {
@@ -12,9 +13,10 @@ function Faucet() {
         }
 
         try {
+            setStatus("Requesting faucet...");
             // Get the signer's address
             const address = await signer.getAddress();
-            // Send a POST request to the server
+            // Send a request to the server
             const response = await fetch('http://158.220.112.255:3000/requestFunds', {
                 method: 'POST',
                 headers: {
@@ -29,14 +31,17 @@ function Faucet() {
 
             const data = await response.json();
             console.log('Response:', data);
+            setStatus("Success!");
         } catch (error) {
             console.error('Error requesting faucet:', error);
+            setStatus("Error requesting faucet");
         }
     };
 
     return (
         <div className="faucet-container">
             <button className="faucet-button" onClick={handleFaucet}>Request Funds</button>
+            {status && <div className="status-message">{status}</div>}
         </div>
     );
 }
